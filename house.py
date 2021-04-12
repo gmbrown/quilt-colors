a53f3e508d216e3e320e6c7d2a108d9904cecbf6
Greg Brown <gbrown@carnegierobotics.com>
2021-04-12 15:13:58 -0400

    basic house class

---
+++ house.py
@@ -0,0 +1,18 @@
+class House:
+    allowed_colors = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+    def __init__(self, wall_color, door_color, roof_color, sky_color):
+        self.color_set = set()
+        for color in [wall_color, door_color, roof_color, sky_color]:
+            if not color in self.allowed_colors:
+                raise Exception(color + " is not one of the allowed colors")
+
+            if color in self.color_set:
+                raise Exception("a house must be all unique colors")
+
+            self.color_set.add(color)
+
+        self.wall_color = wall_color
+        self.door_color = door_color
+        self.roof_color = roof_color
+        self.sky_color = sky_color
